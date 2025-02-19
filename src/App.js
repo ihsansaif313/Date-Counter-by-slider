637b7013aa54a1240872b9ad1336687102bb8a8b
Ihsan Saif <ihsansaifedwardion@gmail.com>
2025-02-20 04:17:01 +0500

    added slider, reset button and logics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  return <DateCounter />;
+}
+function DateCounter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      {/* Floating bubbles */}
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+
+      <div className="counter-container">
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+
+        <div className="control">
+          <span className="value-display">Step: {step}</span>
+        </div>
+
+        <div className="control">
+          <button className="button" onClick={() => setCount((c) => c - step)}>
+            -
+          </button>
+          <span className="value-display">Count: {count}</span>
+          <button className="button" onClick={() => setCount((c) => c + step)}>
+            +
+          </button>
+        </div>
+
+        <p className="date-message">
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today will be`
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {(count !== 0 || step !== 1) && (
+          <div className="control">
+            <button
+              className="button"
+              onClick={() => {
+                setCount(0);
+                setStep(1);
+              }}
+            >
+              RESET
+            </button>
+          </div>
+        )}
+        <h3>IHSAN SAIF</h3>
+      </div>
+    </>
   );
 }
-
-export default App;
